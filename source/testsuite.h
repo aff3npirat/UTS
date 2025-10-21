d73f25bdaff80e834e2440cf409f246f014758c1
aff3npirat <jurek.elliesen@web.de>
2025-10-22 00:27:07 +0200

    Move implementation to .cpp file

--- source/testsuite.h
+++ source/testsuite.h
@@ -26,53 +26,15 @@ std::vector<std::string> testErrors;
 namespace UnitTest {
 namespace Details {
 
-std::vector<value_t>& registered_functions()
-{
-    static std::vector<value_t> functions;
-    return functions;
-}
+std::vector<value_t>& registered_functions();
 
-void register_function(std::string name, func_t function, std::string file, int lnr)
-{
-    std::vector<value_t> funcs = registered_functions();
+void register_function(std::string name, func_t function, std::string file, int lnr);
 
-    if (std::find(funcs.begin(), funcs.end(), name) != funcs.end()) {
-        throw std::invalid_argument("Test name '" + name + "' must be unique, file " + file +
-                                    ", line " + std::to_string(lnr));
-    }
-
-    registered_functions().emplace_back(name, function);
-}
-
-void fail_test(std::string msg)
-{
-    testErrors.push_back(msg);
-}
+void fail_test(std::string msg);
 
 }  // namespace Details
 
-void run_tests()
-{
-    unsigned int numPassed = 0;
-    for (value_t testCase : Details::registered_functions()) {
-        testErrors.clear();
-        testCase.func();
-
-        std::string name = testCase.key;
-        if (testErrors.empty()) {
-            printf("Test %s passed!\n", name.c_str());
-            numPassed++;
-        }
-        else {
-            printf("Test %s failed!\n", name.c_str());
-            for (std::string error : testErrors) {
-                printf("    %s\n", error.c_str());
-            }
-        }
-    }
-
-    printf("%d/%d tests passed\n", numPassed, Details::registered_functions().size());
-}
+void run_tests();
 
 }  // namespace UnitTest
 
